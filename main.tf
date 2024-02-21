7f743255f269a7ac7d4a02843dcbf27ab40e44c0
stevenpicton1979 <steven.picton@gmail.com>
2024-02-21 11:41:23 +1000

    updated gh wf to match version usage 3

--- main.tf
+++ main.tf
@@ -22,23 +22,7 @@ provider "azurerm" {
   features {}
 }
 
-data "azurerm_client_config" "core" {}
-
-module "enterprise_scale" {
-  source  = "Azure/caf-enterprise-scale/azurerm"
-  version = "5.1.0" # not allowed - var.enterprise_scale_version_no  
-  default_location = "australiaeast"
-  providers = {
-    azurerm              = azurerm
-    azurerm.connectivity = azurerm.connectivity
-    azurerm.management   = azurerm.management
-  }
-  root_parent_id = data.azurerm_client_config.core.tenant_id
-  root_id        = "charli-app"
-  root_name      = "Non-Prod"
-  subscription_id_connectivity  = data.azurerm_client_config.connectivity.subscription_id
-  subscription_id_identity  = data.azurerm_client_config.identity.subscription_id
-  subscription_id_management  = data.azurerm_client_config.management.subscription_id
-  deploy_connectivity_resources = true
-  deploy_management_resources = true
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = "australiaeast"
 }
